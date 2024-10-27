19d035bd1048c492987b37b050ccff701f971a9d
Zhang-Lechen <zhanglechen99@163.com>
2024-10-28 00:34:34 +0800

    给portCtrl编写了部分示例代码

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CDLC
+
 source code of compiler CDLC
+
+## 2024.10.27
+
+修改了temp/portCtrl.cpp文件，添加了周期型发送数据的模板函数
+
+事件型接收端口只用到了on_data_available函数，ReceiveMegs函数没用到
+
+周期型接收端口（使用采样周期）只需要修改ReceiveMegs函数中的receive_period就可以了，并修改构造函数
+
+采用消息队列方式的接收端口
--- temp/portCtrl.cpp
+++ temp/portCtrl.cpp
@@ -1,182 +1,26 @@
-#include "djifadjf.h"
+#include "anything.h"
 
-void ECOM::Components::OriginData2::ReceiveMegs() {
-    HelloWorldDataReader *HelloWorld_reader = NULL;
-    DDS_SampleInfoSeq info_seq;
-    HelloWorldSeq data_seq;
-    DDS_ReturnCode_t retcode;
-    
-    HelloWorld_reader = HelloWorldDataReader::narrow(this->reader);
-    if (HelloWorld_reader == NULL) {
-        //fprintf(stderr, "DataReader narrow error");
-        return;
-    }
+// 周期型发送消息的模板函数
+void ECOM::Components::OriginData1::OriginData1SendMegsPeriod()
+{
+	if (pComponent->status==ECOM::Base::Activated)
+	{
 
-    if (this->portType == "event") {
-        // 事件型端口逻辑
-        while (1) {
-            retcode = HelloWorld_reader->take(data_seq, info_seq, DDS_LENGTH_UNLIMITED,
-                                              DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-            if (retcode == DDS_RETCODE_OK) {
-                for (int i = 0; i < data_seq.length(); ++i) {
-                    if (info_seq[i].valid_data) {
-                        ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread = 
-                            new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                        pPortThread->pPort = this;
-                        pPortThread->instance = data_seq[i];
-
-                        boost::thread *pThread;
-                        pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                    }
-                }
-            }
-            HelloWorld_reader->return_loan(data_seq, info_seq);
-        }
-    } else if (this->portType == "periodic") {
-        // 周期型端口逻辑
-        if (this->portModifier == "queue") {
-            // 消息队列方式，处理消息队列长度
-            std::queue<HelloWorld> messageQueue;
-
-            while (1) {
-                retcode = HelloWorld_reader->take(data_seq, info_seq, DDS_LENGTH_UNLIMITED,
-                                                  DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-                if (retcode == DDS_RETCODE_OK) {
-                    for (int i = 0; i < data_seq.length(); ++i) {
-                        if (info_seq[i].valid_data) {
-                            if (messageQueue.size() >= this->queueLength) {
-                                messageQueue.pop(); // 如果队列已满，丢弃旧消息
-                            }
-                            messageQueue.push(data_seq[i]);
-                        }
-                    }
-                }
-                HelloWorld_reader->return_loan(data_seq, info_seq);
-
-                // 处理队列中的消息
-                while (!messageQueue.empty()) {
-                    HelloWorld instance = messageQueue.front();
-                    messageQueue.pop();
-                    ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread =
-                        new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                    pPortThread->pPort = this;
-                    pPortThread->instance = instance;
-
-                    boost::thread *pThread;
-                    pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                }
-            }
-        } else if (this->portModifier == "sampling") {
-            // 采样方式，基于给定的采样周期
-            while (1) {
-                // 睡眠指定的采样周期
-                boost::this_thread::sleep(boost::posix_time::seconds(this->samplePeriod.sec));
-
-                retcode = HelloWorld_reader->take(data_seq, info_seq, 1,  // 只取一个最新的样本
-                                                  DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-                if (retcode == DDS_RETCODE_OK) {
-                    if (info_seq[0].valid_data) {
-                        ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread =
-                            new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                        pPortThread->pPort = this;
-                        pPortThread->instance = data_seq[0];
-
-                        boost::thread *pThread;
-                        pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                    }
-                }
-                HelloWorld_reader->return_loan(data_seq, info_seq);
-            }
-        }
-    }
+		// 设置发送周期，第一个参数是秒，第二个参数是纳秒
+		struct DDS_Duration_t send_period = {this->period/1E3,this->period%1000*1E6};
+		
+		while (true)
+		{
+  
+			/***********************************************
+	        * TODO: put your logic business code here
+	        * ***********************************************/
+			// 等待下一个周期
+			// NDDSUtility::sleep(send_period);
+			std::this_thread::sleep_for(std::chrono::seconds(send_period.sec) + std::chrono::nanoseconds(send_period.nanosec));
+		}
+	}
 }
-void ECOM::Components::OriginData2::ReceiveMegs() {
-    HelloWorldDataReader *HelloWorld_reader = NULL;
-    DDS_SampleInfoSeq info_seq;
-    HelloWorldSeq data_seq;
-    DDS_ReturnCode_t retcode;
-    
-    HelloWorld_reader = HelloWorldDataReader::narrow(this->reader);
-    if (HelloWorld_reader == NULL) {
-        //fprintf(stderr, "DataReader narrow error");
-        return;
-    }
 
-    if (this->portType == "event") {
-        // 事件型端口逻辑
-        while (1) {
-            retcode = HelloWorld_reader->take(data_seq, info_seq, DDS_LENGTH_UNLIMITED,
-                                              DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-            if (retcode == DDS_RETCODE_OK) {
-                for (int i = 0; i < data_seq.length(); ++i) {
-                    if (info_seq[i].valid_data) {
-                        ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread = 
-                            new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                        pPortThread->pPort = this;
-                        pPortThread->instance = data_seq[i];
+// 周期型接收消息的模板函数：使用消息队列
 
-                        boost::thread *pThread;
-                        pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                    }
-                }
-            }
-            HelloWorld_reader->return_loan(data_seq, info_seq);
-        }
-    } else if (this->portType == "periodic") {
-        // 周期型端口逻辑
-        if (this->portModifier == "queue") {
-            // 消息队列方式，处理消息队列长度
-            std::queue<HelloWorld> messageQueue;
-
-            while (1) {
-                retcode = HelloWorld_reader->take(data_seq, info_seq, DDS_LENGTH_UNLIMITED,
-                                                  DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-                if (retcode == DDS_RETCODE_OK) {
-                    for (int i = 0; i < data_seq.length(); ++i) {
-                        if (info_seq[i].valid_data) {
-                            if (messageQueue.size() >= this->queueLength) {
-                                messageQueue.pop(); // 如果队列已满，丢弃旧消息
-                            }
-                            messageQueue.push(data_seq[i]);
-                        }
-                    }
-                }
-                HelloWorld_reader->return_loan(data_seq, info_seq);
-
-                // 处理队列中的消息
-                while (!messageQueue.empty()) {
-                    HelloWorld instance = messageQueue.front();
-                    messageQueue.pop();
-                    ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread =
-                        new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                    pPortThread->pPort = this;
-                    pPortThread->instance = instance;
-
-                    boost::thread *pThread;
-                    pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                }
-            }
-        } else if (this->portModifier == "sampling") {
-            // 采样方式，基于给定的采样周期
-            while (1) {
-                // 睡眠指定的采样周期
-                boost::this_thread::sleep(boost::posix_time::seconds(this->samplePeriod.sec));
-
-                retcode = HelloWorld_reader->take(data_seq, info_seq, 1,  // 只取一个最新的样本
-                                                  DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
-                if (retcode == DDS_RETCODE_OK) {
-                    if (info_seq[0].valid_data) {
-                        ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData *pPortThread =
-                            new ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData;
-                        pPortThread->pPort = this;
-                        pPortThread->instance = data_seq[0];
-
-                        boost::thread *pThread;
-                        pThread = new boost::thread(boost::bind(&ECOM::Port::OriginalData::Consume::ThreadForConsumePortOriginalData::invoke, pPortThread));
-                    }
-                }
-                HelloWorld_reader->return_loan(data_seq, info_seq);
-            }
-        }
-    }
-}
